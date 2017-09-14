fcfa20a82e1c12aaaa1b7d8a5920ac0bbd8f03db
Mallikarjuna Kalkere <mkalkere@gmail.com>
2017-09-14 14:25:08 -0400

    Add project files.

---
+++ INotifyPropertyChanged-Style1/CustomCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INotifyPropertyChanged_Style1
+{
+    public class CustomCommand : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+        private ICommand click;
+        private Func<object, bool> canClick;
+
+        public CustomCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public CustomCommand(ICommand click, Func<object, bool> canClick)
+        {
+            this.click = click;
+            this.canClick = canClick;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ INotifyPropertyChanged-Style1/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INotifyPropertyChanged_Style1
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public MainWindowViewModel()
+        {
+
+            InitializeCommands();
+            Name = "Initial Name";
+        }
+
+        #region Properties
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                RaisePropertyChanged(value, ref _name);
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged<T>(T value, ref T field, [CallerMemberName] string propertyName = "")
+        {
+            if (!Equals(value, field))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region ICommand
+        public ICommand Click { get; set; }
+
+        private void InitializeCommands()
+        {
+            Click = new CustomCommand(OnClick, CanClick);
+        }
+
+        private void OnClick(object obj)
+        {
+            Name = "Changed Name " + new Random().Next(1, 100);
+        }
+
+        private bool CanClick(object obj) => true;
+        #endregion
+    }
+}
